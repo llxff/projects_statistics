05ee1ce8883b68302d269e17dc8d84d9b81c0607
Александр Фомин <ll.wg.bin@gmail.com>
2017-01-14 16:44:29 +0300

    show token

--- web/static/js/views/home/index.js
+++ web/static/js/views/home/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class HomeIndexView extends React.Component {
   render() {
     return (
-      <div></div>
+      <div>{ localStorage.getItem("csrfToken") }</div>
     );
   }
 }

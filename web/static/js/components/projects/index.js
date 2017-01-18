d34e5831f000d122be294fa93597362de7c19abc
Александр Фомин <ll.wg.bin@gmail.com>
2017-01-18 17:11:34 +0300

    projects component

---
+++ web/static/js/components/projects/index.js
@@ -0,0 +1,12 @@
+import React       from "react";
+import { connect } from 'react-redux';
+
+class Projects extends React.Component {
+  render() {
+    <div><p>Проекты</p></div>
+  }
+}
+
+const mapStateToProps = (state) => ({});
+
+export default connect(mapStateToProps)(Projects);
